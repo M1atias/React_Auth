550eb8192175ddee1954770071805cab6b807ab8
M1atias <maty.f.molina4a@gmail.com>
2021-11-17 16:04:41 -0300

    edit .env

--- react-auth0-intro/src/index.js
+++ react-auth0-intro/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from '@auth0/auth0-react'
 
+
+console.log(
+  process.env.REACT_APP_AUTH0_DOMAIN,
+  process.env.REACT_APP_AUTH0_CLIENT_ID
+)
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider>
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
